9a41e38fa74c054ffffcceef3c5a2a6e13278e33
coldcss <coldarif1@gmail.com>
2026-02-10 14:15:34 +0600

    Update story.py
    As we can see, random.choice() function basically selects an item from a list of items. 
    
    Following are the steps involved in this Random story generator project.
    
    1. Import the random module, as it is a built-in module of python.  So, there's no need to install it manually.
    
    
    # Importing random module
    import random
    2.  Define several lists of phrases. Here, we have defined eight lists. We can define more also, it depends totally on our choice.
    
    Sentence_starter - This list gives an idea about the time of the event.
    character - This list tells about the main character of this story.
    time - This list defines the exact day on which some incident has occurred.
    story_plot - This list defines the plot of the story.
    place - This list defines the place at which the incident occurred.
    second_character - This list defines the second character of the story.
    age - This list defines the age of the second character.
    work - This list tells about the work the second character was doing.
    
    # Defining list of phrases which will help to build a story

--- story.py
+++ story.py
@@ -8,11 +8,19 @@ character        = [' there lived a king.',' there was a man named Jack.', ' the
 time             = [' One day', ' One full-moon night']
 story_plot       = [' he was passing by',' he was going for a picnic to ']
 
-
+place            = [' the mountains', ' the garden']
+second_character = [' he saw a man', ' he saw a young lady']
+age              = [' who seemed to be in late 20s', ' who seemed very old and feeble']
+work             = [' searching something.', ' digging a well on roadside.']
 
 # Selecting an item from each list and concatenating them.
 print(random.choice(Sentence_starter)+random.choice(character)+
       random.choice(time)+random.choice(story_plot) +
       random.choice(place)+random.choice(second_character)+
-
       random.choice(age)+random.choice(work))
+
+#In the 20 BC there was a man named Jack. One full-moon night he was passing 
+#by the garden he saw a young lady 
+#who seemed very old and feeble searching something.
+
+
